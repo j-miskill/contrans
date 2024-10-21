e90ab3e67421e3420447d08932b24c5fbaca5d29
j-miskill <jcmiskill@gmail.com>
2024-10-21 12:17:33 -0400

    classwork from 10.21.2024

--- contrans.py
+++ contrans.py
@@ -12,15 +12,38 @@ class contrans:
         self.mypassword = os.getenv('mypassword')
         self.news_api_key = os.getenv("NEWS_API_KEY")
         self.congress_api_key = os.getenv("CONGRESS_API_KEY")
+        self.us_state_to_abbrev = {
+                        "Alabama": "AL","Alaska": "AK","Arizona": "AZ","Arkansas": "AR",
+                        "California": "CA","Colorado": "CO","Connecticut": "CT","Delaware": "DE",
+                        "Florida": "FL","Georgia": "GA","Hawaii": "HI",
+                        "Idaho": "ID","Illinois": "IL","Indiana": "IN","Iowa": "IA",
+                        "Kansas": "KS","Kentucky": "KY","Louisiana": "LA",
+                        "Maine": "ME","Maryland": "MD","Massachusetts": "MA",
+                        "Michigan": "MI","Minnesota": "MN","Mississippi": "MS",
+                        "Missouri": "MO","Montana": "MT","Nebraska": "NE",
+                        "Nevada": "NV","New Hampshire": "NH","New Jersey": "NJ",
+                        "New Mexico": "NM","New York": "NY","North Carolina": "NC",
+                        "North Dakota": "ND","Ohio": "OH","Oklahoma": "OK",
+                        "Oregon": "OR","Pennsylvania": "PA","Rhode Island": "RI",
+                        "South Carolina": "SC","South Dakota": "SD","Tennessee": "TN",
+                        "Texas": "TX","Utah": "UT","Vermont": "VT",
+                        "Virginia": "VA","Washington": "WA","West Virginia": "WV",
+                        "Wisconsin": "WI","Wyoming": "WY","District of Columbia": "DC",
+                        "American Samoa": "AS","Guam": "GU","Northern Mariana Islands": "MP",
+                        "Puerto Rico": "PR","United States Minor Outlying Islands": "UM",
+                        "Virgin Islands": "VI"
+                        }
 
     
     def get_votes(self):
         url = "https://voteview.com/static/data/out/votes/H118_votes.csv"
+        
         votes = pd.read_csv(url)
         return votes
 
     def get_ideology(self):
-        url = "https://voteview.com/static/data/out/votes/H118_members.csv"
+        # url = "https://voteview.com/static/data/out/votes/H118_members.csv"
+        url = "https://voteview.com/static/data/out/members/H118_members.csv"
         members = pd.read_csv(url)
         return members
 
@@ -140,6 +163,52 @@ class contrans:
         headers = self.make_headers()
         r = requests.get(params=params, url=base+endpoint, headers=headers)
         return json.loads(r.text)
+    
+
+    def make_cand_table(self):
+                members = self.get_bioguideIDs()
+                replace_map = {'Republican': 'R','Democratic': 'D','Independent': 'I'}
+                members['partyletter'] = members['partyName'].replace(replace_map)
+                members['state'] = members['state'].replace(self.us_state_to_abbrev)
+                members['district'] = members['district'].fillna(0)
+                members['district'] = members['district'].astype('int').astype('str')
+                members['district'] = ['0' + x if len(x) == 1 else x for x in members['district']]
+                members['district'] = [x.replace('00', 'S') for x in members['district']]
+                members['DistIDRunFor'] = members['state']+members['district']
+                members['lastname']= [x.split(',')[0] for x in members['name']]
+                members['firstname']= [x.split(',')[1] for x in members['name']]
+                members['name2'] = [ y.strip() + ' (' + z.strip() + ')' 
+                                for y, z in 
+                                zip(members['lastname'], members['partyletter'])]
+                
+                cands = pd.read_csv('data/cands22.txt', quotechar="|", header=None)
+                cands.columns = ['Cycle', 'FECCandID', 'CID','FirstLastP',
+                                'Party','DistIDRunFor','DistIDCurr',
+                                'CurrCand','CycleCand','CRPICO','RecipCode','NoPacs']
+                cands['DistIDRunFor'] = [x.replace('S0', 'S') for x in cands['DistIDRunFor']]
+                cands['DistIDRunFor'] = [x.replace('S1', 'S') for x in cands['DistIDRunFor']]
+                cands['DistIDRunFor'] = [x.replace('S2', 'S') for x in cands['DistIDRunFor']]
+                cands['name2'] = [' '.join(x.split(' ')[-2:]) for x in cands['FirstLastP']]
+                cands = cands[['CID', 'name2', 'DistIDRunFor']].drop_duplicates(subset=['name2', 'DistIDRunFor'])
+                crosswalk = pd.merge(members, cands, 
+                     left_on=['name2', 'DistIDRunFor'],
+                     right_on=['name2', 'DistIDRunFor'],
+                     how = 'inner')
+                return crosswalk
+    
+
+    def terms_df(self, members):
+        termsDF = pd.DataFrame()
+        for index, row in members.iterrows():
+            bioguideId = row['bioguideId']
+            terms = row['terms.item']
+            df = pd.DataFrame.from_records(terms)
+            df['bioguideId'] = bioguideId
+            termsDF = pd.concat([termsDF, df])
+        members = members.drop("terms.item", axis=1)
+        return termsDF, members
+
+
 
 
 
