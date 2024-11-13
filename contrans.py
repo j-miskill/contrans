f0bc893a9e8b132e52637eac5d06ca015ce63a13
j-miskill <jcmiskill@gmail.com>
2024-11-13 12:17:26 -0500

    adding in changes from class 11.13.2024.

--- contrans.py
+++ contrans.py
@@ -322,9 +322,16 @@ class contrans:
         mongo_bills.insert_many(bill_list_with_text)
 
     def upload_many_members_to_mongo(self, mongo_bills: pymongo.MongoClient, members: list):
+        i = 1
         for m in members:
-            print(f'Now uploading bills from {m} to MongoDB')
-            self.upload_one_member_to_mongo(mongo_bills=mongo_bills, bioguideid=m)
+            try:
+                print('uploading bills from {m} to MongoDB: legislator {i} of {len(members)}')
+                self.upload_one_member_to_mongo(mongo_bills=mongo_bills, bioguideid=m)
+            except Exception as e:
+                print(f"Process failed for: {m} on the {i} index.")
+                print(e)
+                continue
+            i += 1
 
     def query_mongo(self, collection, rows, columns):
         cursor = collection.find(rows, columns)
